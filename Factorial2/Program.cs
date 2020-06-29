2318f9e750ae198b45efc540a68b3218a6a90a3e
Alex <alex3kot@outlook.com>
2020-06-29 22:58:32 +0400

    Добавьте файлы проекта.

---
+++ Factorial2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factorial2
+{
+    class Program
+    {
+        public static long Factorial(int N)
+        {
+            long factotial = 1;
+            if (N != 0)
+            {
+                for (int i = 2; i <= N; i++)
+                {
+                    factotial *= i;
+                }
+            }
+            return factotial;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int N = Int32.Parse(Console.ReadLine());
+            if (N >= 0)
+            {
+                Console.WriteLine($"Факториал числа {N}: {Factorial(N)}");
+            }
+            else
+            {
+                Console.WriteLine("Было введено отрицательное число!");
+            }
+        }
+    }
+}
